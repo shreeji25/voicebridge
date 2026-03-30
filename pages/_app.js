7f6e240d09f2f8845f29f43333d1b80467fc720b
shreeji25@ <shreejijansari25@gmail.com>
2026-03-30 13:30:51 +0530

    update

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-// pages/_app.js
-// This is the root wrapper for every page in Next.js.
-// We import our global CSS here so it applies everywhere.
-
-import '../styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
